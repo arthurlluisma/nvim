1a37dd690b366a8b4fdd724e55e4bcf85807b6a2
Arthur Lluisma <ajclluisma@gmail.com>
2026-01-18 22:18:16 +0800

    chore: update packages

--- init.lua
+++ init.lua
@@ -920,28 +920,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('catppuccin').setup {
-        styles = {
-          comments = {},
+      require('gruvbox').setup {
+        italic = {
+          strings = false,
+          comments = false,
         },
-        integrations = {
-          cmp = true,
-          harpoon = true,
-          mason = true,
-          native_lsp = { enabled = true },
-          telescope = true,
-          treesitter = true,
-        },
+        transparent_mode = false,
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
 
